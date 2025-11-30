c034f91a0e3b70ccde929a91455fcf6a6b04a328
WhiteLava <dev@nethide.dev>
2025-11-30 10:55:37 +0300

    botd_declination -> members_declination

--- utils/members_declination.py
+++ utils/members_declination.py
@@ -1,7 +1,7 @@
-async def bots_plural(number: int) -> str:
+async def members_plural(number: int) -> str:
     if number % 10 == 1 and number % 100 != 11:
-        return f"{number} бот"
+        return f"{number} участник"
     elif 2 <= number % 10 <= 4 and (number % 100 < 10 or number % 100 >= 20):
-        return f"{number} бота"
+        return f"{number} участника"
     else:
-        return f"{number} ботов"
+        return f"{number} участников"
