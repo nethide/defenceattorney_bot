5b87b0c0130782adbefdbd5fbeb3cd40a4a2968d
WhiteLava <dev@nethide.dev>
2025-12-11 19:09:49 +0300

    Added apscheduler for kick

--- bot.py
+++ bot.py
@@ -6,10 +6,13 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.types import  BotCommand
+from aiogram.types import Chat, ChatMember
+from apscheduler.jobstores.redis import RedisJobStore
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler_di import ContextSchedulerDecorator
 
 # Конфиг
-from config import TOKEN
+from config import TOKEN, REDIS
 
 logging.basicConfig(level=logging.INFO, handlers=[
     logging.StreamHandler()
@@ -18,11 +21,28 @@ bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
 # Роутеры (Routers)
 from handlers.moderation import router as execute_router
-from handlers.on_join_notify import router as on_join_notify
+from handlers.on_join_captcha import router as on_join_notify
+
+from middlewares.SchedulerMiddleware import SchedulerMiddleware
 
 async def main():
+    jobstores = {
+        'default': RedisJobStore(jobs_key='dispatched_trips_jobs',
+                                 run_times_key='dispatched_trips_running',
+                                 db=2,
+                                 port=6379)
+    }
+    if REDIS:
+        scheduler = ContextSchedulerDecorator(AsyncIOScheduler(timezone="Europe/Moscow", jobstores=jobstores))
+    else:
+        scheduler = ContextSchedulerDecorator(AsyncIOScheduler(timezone="Europe/Moscow"))
+
+    scheduler.ctx.add_instance(bot, declared_class=Bot)
+    scheduler.start()
+
     dp = Dispatcher()
     dp.include_routers(execute_router, on_join_notify)
+    dp.update.middleware(SchedulerMiddleware(scheduler))
     await asyncio.gather(dp.start_polling(bot))
 
 if __name__ == '__main__':
---
+++ middlewares/SchedulerMiddleware.py
@@ -0,0 +1,19 @@
+from typing import Dict, Any, Callable, Awaitable
+
+from aiogram import BaseMiddleware
+from aiogram.types.base import TelegramObject
+from apscheduler_di import ContextSchedulerDecorator
+
+
+class SchedulerMiddleware(BaseMiddleware):
+    def __init__(self, scheduler: ContextSchedulerDecorator):
+        self.scheduler = scheduler
+
+    async def __call__(
+            self,
+            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+            event: TelegramObject,
+            data: Dict[str, Any]
+    ) -> Any:
+        data['scheduler'] = self.scheduler
+        return await handler(event, data)
