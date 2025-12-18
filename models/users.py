43927d7676e1516d4855dfb31909c0c41c68d727
WhiteLava <dev@nethide.dev>
2025-12-18 09:46:40 +0300

    new model users for database work

--- models/users.py
+++ models/users.py
@@ -1,11 +1,12 @@
 from asyncpg.pool import Pool
 from aiogram import Bot
+from aiogram.types import ChatMember
 from datetime import datetime
 from typing import Any
 
 class User:
     @classmethod
-    async def load_data(cls, pool: Pool, telegram_id: int, group_id: int):
+    async def get_data(cls, bot: Bot, pool: Pool, telegram_id: int, group_id: int):
         is_user_registered_global = await pool.fetchval(
         """
         SELECT EXISTS(
@@ -36,7 +37,7 @@ class User:
         """,
         telegram_id, group_id)
 
-        if not is_user_registered_local:
+        if is_user_registered_local is False:
             await pool.execute(
                 """
                 INSERT INTO user_groups(telegram_id, group_id)
@@ -48,42 +49,51 @@ class User:
         data = await pool.fetchrow(
             """
             SELECT * 
-            FROM users_groups
+            FROM user_groups
             WHERE telegram_id = $1
             and group_id = $2
             """,
             telegram_id, group_id
         )
 
-        return cls(pool, telegram_id, group_id, data)
+        tg_data = await bot.get_chat_member(group_id, telegram_id)
 
-    def __init__(self, pool: Pool, telegram_id: int, group_id: int, data: dict):
+        return cls(bot, pool, telegram_id, group_id, data, tg_data)
+
+    def __init__(self, bot: Bot, pool: Pool, telegram_id: int, group_id: int, data: dict, tg_data: ChatMember):
         self.pool = pool
         self.telegram_id = telegram_id
         self.data = data
+        self.group_id = group_id
+        self.bot = bot
+        self.tg_data = tg_data
 
-    async def telegram_id(self):
+    def telegram_id(self):
         return self.telegram_id
 
-    async def group_id(self):
+    def group_id(self):
         return self.data['group_id']
 
-    async def warnings(self):
+    @property
+    def warnings(self):
         return self.data["warnings_count"]
 
-    async def role(self):
+    @property
+    def role(self):
         return self.data["role"]
 
-    async def is_banned(self):
+    @property
+    def is_banned(self):
         return self.data["is_banned"]
 
-    async def ban_reason(self):
+    @property
+    def ban_reason(self):
         return self.data["ban_reason"]
 
     async def ban_user(self, reason: str, until: Any, moderator_id: str):
         await self.pool.execute(
             """
-            UPDATE users_groups
+            UPDATE user_groups
             SET is_banned = true
             AND ban_reason = $1
             WHERE telegram_id = $2
@@ -91,12 +101,18 @@ class User:
             """, reason, self.telegram_id, self.group_id
         )
 
+        print(until)
+
+        if not until:
+            until = None
+        else:
+            until = until.replace(tzinfo=None)
+
         await self.pool.execute(
             """
             INSERT INTO moderator_log(group_id, user_id, action, reason, moderator_id, until)
             VALUES($1, $2, $3, $4, $5, $6)
-            """, self.data['group_id'], self.data['user_id'], 'ban', reason, moderator_id,
-            datetime.fromtimestamp(until if until else None)
+            """, self.data['group_id'], self.data['telegram_id'], 'ban', reason, moderator_id, until
         )
 
     async def unban_user(self, reason: str, until: Any, moderator_id: str):
@@ -153,6 +169,31 @@ class User:
             WHERE captcha_completed is false
             """
         )
+
+    @property
+    def is_user_admin(self):
+        if self.tg_data is None:
+            return False
+        return self.tg_data.status in ["administrator", "creator"]
+
+    @property
+    def username(self):
+        if self.tg_data.user.username is None:
+            return ""
+        return self.tg_data.user.username
+
+    @property
+    def full_name(self):
+        if self.tg_data.user.full_name is None:
+            return ""
+        return self.tg_data.user.full_name
+
+    @property
+    def status(self):
+        return self.tg_data.status
+
+
+
 
 
 
